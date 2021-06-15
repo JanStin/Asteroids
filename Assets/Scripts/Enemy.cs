debc4747035dd9fadcbfb20fba185de7b2dc33a8
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2021-06-15 22:22:29 +0300

    feat: created enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    private float speed = 3.0f;
+
+    private void Update()
+    {
+        transform.position += (player.position - transform.position).normalized * speed * Time.deltaTime;
+    }    
+}
