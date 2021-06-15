9df922cc341a0b0f46df6e34024c7b22aa3f9eea
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2021-06-15 21:09:46 +0300

    refactor: replace destroy method asteroid on trigger with Projectile tag

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -13,16 +13,19 @@ public class Asteroid : SpaceObject
         Destroy(gameObject, lifeTime);
     }
 
-    private void OnDestroy()
-    {        
-        int countSplinters = 3;
-        for (int i = 0; i < countSplinters; i++)
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Projectile"))
         {
-            System.Random random = new System.Random();
-            // TODO: Сделать более рандомный раскол.
-            GameObject tempSplinter = Instantiate(splinter, transform.position, Quaternion.identity);
-            Rigidbody splinterRigidbody = tempSplinter.GetComponent<Rigidbody>();
-            splinterRigidbody.velocity = new Vector3(0, Random.Range(-1, 1), 0);
+            int countSplinters = 3;
+            for (int i = 0; i < countSplinters; i++)
+            {
+                System.Random random = new System.Random();
+                // TODO: Сделать более рандомный раскол.
+                GameObject tempSplinter = Instantiate(splinter, transform.position, Quaternion.identity);
+                Rigidbody splinterRigidbody = tempSplinter.GetComponent<Rigidbody>();
+                splinterRigidbody.velocity = new Vector3(0, Random.Range(-1, 1), 0);
+            }
         }
-    }    
+    }
 }
