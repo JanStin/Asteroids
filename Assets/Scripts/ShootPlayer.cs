07250a12818254d5b470d345d20489a4c1b11727
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2021-06-16 19:51:49 +0300

    feat: cooldown for laser

--- Assets/Scripts/ShootPlayer.cs
+++ Assets/Scripts/ShootPlayer.cs
@@ -8,6 +8,10 @@ public class ShootPlayer : MonoBehaviour
     [SerializeField] private GameObject laser;
     [SerializeField] private Transform firePoint;
 
+    private float cooldownLaser = 10.0f;
+    private float rechargeTimeOneShoot;
+    private float rechargeTimeTwoShoot;
+
     private void Update()
     {
         Shoot();   
@@ -23,10 +27,28 @@ public class ShootPlayer : MonoBehaviour
             Rigidbody bulletRigidbody = tempBullet.GetComponent<Rigidbody>();
             bulletRigidbody.velocity = 12.0f * firePoint.transform.TransformVector(new Vector3(0, 1, 0));
         }
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(1) && !CooldownLaser())
         {
             Vector3 laserPosition = new Vector3(firePoint.position.x, firePoint.position.y + 6, 0);
             Instantiate(laser, laserPosition, firePoint.rotation);
         }
+        rechargeTimeOneShoot += Time.deltaTime;
+        rechargeTimeTwoShoot += Time.deltaTime;
+    }
+
+    private bool CooldownLaser()
+    {
+        if (rechargeTimeOneShoot > cooldownLaser)
+        {
+            rechargeTimeOneShoot = 0;
+            return false;
+        }
+        else if (rechargeTimeTwoShoot > cooldownLaser)
+        {
+            rechargeTimeTwoShoot = 0;
+            return false;
+        }
+
+        return true;
     }
 }
