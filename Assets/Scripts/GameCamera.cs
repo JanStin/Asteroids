d1b277d6b15bc45e595acf7b81d58ea378afb20f
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2021-06-17 12:04:52 +0300

    feat: change view

--- Assets/Scripts/GameCamera.cs
+++ Assets/Scripts/GameCamera.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameCamera : MonoBehaviour
 {
     private static Vector2 size;
+    private Camera camera;
     public static Vector2 Size
     { 
         get
@@ -16,8 +17,27 @@ public class GameCamera : MonoBehaviour
 
     private void Start()
     {
-        var camera = Camera.main;
+        camera = Camera.main;
         size = new Vector2(camera.orthographicSize * camera.aspect, 
             camera.orthographicSize * camera.aspect);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            if (camera.orthographic)
+            {
+                camera.orthographic = false;
+                gameObject.transform.position = new Vector3(0, -11, -3);
+                gameObject.transform.Rotate(-75, 0, 0);
+            }
+            else
+            {
+                camera.orthographic = true;
+                gameObject.transform.position = new Vector3(0, 0, -10);
+                gameObject.transform.rotation = new Quaternion(0, 0, 0, 0);
+            }
+        }
     }
 }
